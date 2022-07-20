d30edca3f1dadad6520e31a449874d664fe6f88d
han14777 <1043404247@qq.com>
2022-07-20 13:26:44 +0800

    Update umu.js

--- umu.js
+++ umu.js
@@ -1,25 +1,29 @@
-//like
-var p=$$("#comment-list>div")
-for(var i=0;i<p.length;i++){
-var did=p[i].getAttribute("data-id")
-console.log(did);
-var httpRequest = new XMLHttpRequest();
-httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/like/save', true); 
-httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-httpRequest.send('obj_type=4&obj_id='+did+'&action=like');
-
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        var json = httpRequest.responseText;
-        console.log(json);
-    }
-};
-}
 /*
+//like
+var comment_list=$$("#comment-list>div")
+for(var i=0;i<comment_list.length;i++){
+    var did=comment_list[i].getAttribute("data-id")
+    console.log(did);
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/like/save', true); 
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    httpRequest.send('obj_type=4&obj_id='+did_like+'&action=like');
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = httpRequest.responseText;
+            console.log(json);
+            
+        }
+    };
+    }
+console.log("you have got:"+i+"*2="+2*i+"points")
+
+
 var tmplate=document.getElementById('__pageDataTemplate__').innerHTML;
 var jsdata=JSON.parse(tmplate)
 var eid=jsdata.data.element_info.element_id
-*/
+
 //comment
 var tmplate=$$('#__pageDataTemplate__')[0].innerHTML
 var jsdata=JSON.parse(tmplate)
@@ -27,23 +31,54 @@ var eid=jsdata.data.element_info.element_id
 
 var p=$$("#comment-list>div")
 for(var i=0;i<p.length;i++){
-var did=p[i].getAttribute("data-id")
+    var did=p[i].getAttribute("data-id")
 
-console.log(did);
-var httpRequest = new XMLHttpRequest();
-httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/comment/save', true); 
-httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-httpRequest.send('show_parent_id='+did+'&comment_parent_id='+did+'&parent_type=4&parent_id='+eid+'&comment_type=1&comment=aaa');
+    console.log(did);
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/comment/save', true); 
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    httpRequest.send('show_parent_id='+did+'&comment_parent_id='+did+'&parent_type=4&parent_id='+eid+'&comment_type=1&comment=aaa');
 
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        var json = httpRequest.responseText;
-        console.log(json);
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = httpRequest.responseText;
+            console.log(json);
+        
+        }
+    };
     }
-};
-}
+console.log("you have got:"+i+"*5="+5*i+"points")
 
 //show_parent_id=22156246&comment_parent_id=29001448&parent_type=4&parent_id=34524870&comment_type=1&comment=nihao
 //show_parent_id=22181991&comment_parent_id=33358819&parent_type=4&parent_id=34524870&comment_type=1&comment=yse
 
+*/
+//like & comment with aaa
+var tmplate=$$('#__pageDataTemplate__')[0].innerHTML
+var jsdata=JSON.parse(tmplate)
+var eid=jsdata.data.element_info.element_id
+var comment_list=$$("#comment-list>div")
+
+for(var i=0;i<comment_list.length;i++){
+    var did=comment_list[i].getAttribute("data-id")
+    console.log(did);
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/like/save', true); 
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    httpRequest.send('obj_type=4&obj_id='+did+'&action=like');
+
+    httpRequest.open('POST', 'https://m.umu.cn/uapi/v1/comment/save', true); 
+    httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    httpRequest.send('show_parent_id='+did+'&comment_parent_id='+did+'&parent_type=4&parent_id='+eid+'&comment_type=1&comment=aaa');
+
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var json = httpRequest.responseText;
+            console.log(json);
+            
+        }
+    };
+    }
+console.log("you have got:"+i+"*7="+7*i+"points")
 
